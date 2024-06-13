bcc9c59d96928e1f81938d41b408d134aa250edd
marinee080 <nuaim0113@gmail.com>
2024-06-13 10:17:34 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+function mul() {
+    let number1 = document.getElementById("number1");
+    let result = document.getElementById("result");
+    let sum = parseFloat(number1.value) *82.109 ;
+    result.innerHTML = sum;
+}
